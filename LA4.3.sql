cdae0bbe7570a9c18a059be57ca4f3c05b53a039
j-lindsey <j-lindsey@users.noreply.github.com>
2020-08-25 12:03:58 -0600

    adding new files

--- LA4.3.sql
+++ LA4.3.sql
@@ -3,3 +3,35 @@
 SELECT * 
 FROM Vendors INNER JOIN Invoices
 ON Vendors.VendorID = Invoices.VendorID;
+
+--2.
+SELECT VendorName, InvoiceNumber, InvoiceDate, InvoiceTotal-(PaymentTotal + CreditTotal) AS Balance
+FROM Vendors V INNER JOIN Invoices I
+ON V.VendorID = I.VendorID
+WHERE InvoiceTotal-(PaymentTotal + CreditTotal) > 0
+ORDER BY VendorName;
+
+--3.
+SELECT VendorName, DefaultAccountNo, AccountDescription
+FROM Vendors V INNER JOIN GLAccounts G
+ON V.DefaultAccountNo = G.AccountNo
+ORDER BY AccountDescription, VendorName;
+
+--4.
+SELECT V.VendorName AS Vendor, I.InvoiceDate AS Date, I.InvoiceNumber AS Number, LI.InvoiceSequence AS #, LI.InvoiceLineItemAmount AS LineITem
+FROM Vendors V INNER JOIN Invoices I
+ON V.VendorID = I.VendorID
+INNER JOIN InvoiceLineItems LI
+ON I.InvoiceID = LI.InvoiceID
+ORDER BY VendorName, InvoiceDate, InvoiceNumber, InvoiceSequence;
+
+--5.
+SELECT VendorName FROM Vendors
+UNION
+SELECT VendorState,
+CASE 
+	WHEN VendorState = 'CA' THEN 'CA'
+	ELSE 'Outside CA'
+END
+FROM Vendors
+ORDER BY VendorName;
