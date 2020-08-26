829d09dc5216230bfa9bc210d8c38bcd1d57751a
j-lindsey <j-lindsey@users.noreply.github.com>
2020-08-25 22:01:15 -0600

    adding files

---
+++ LA4.6.sql
@@ -0,0 +1,24 @@
+--Learning Activity 4.6
+
+CREATE DATABASE Film_World;
+
+CREATE TABLE Movie(
+	movieid int PRIMARY KEY,
+	title varchar(255) NOT NULL,
+	companyid int FOREIGN KEY REFERENCES Company(companyid),
+	releaseDate date,
+	boxOfficeRevenue decimal,
+	reviewScore float CHECK (reviewScore >=0 AND reviewScore <=10)
+);
+
+CREATE TABLE Actor(
+	actorid int PRIMARY KEY,
+	firstName varchar(255) NOT NULL,
+	lastName varchar(255) NOT NULL,
+	dateOfBirth date
+);
+
+CREATE TABLE ActedIn(
+	movieid int FOREIGN KEY REFERENCES Movie(movieid),
+	actorid int FOREIGN KEY REFERENCES Actor(actorid)
+);
