de1142909116e60238ab8e42116251d1ba974b79
j-lindsey <j-lindsey@users.noreply.github.com>
2020-08-26 16:12:06 -0600

    changes

--- LA4.7.sql
+++ LA4.7.sql
@@ -13,3 +13,28 @@ GROUP BY VendorName
 ORDER BY SUM(PaymentTotal) DESC;
 
 --3.
+SELECT VendorName, COUNT(InvoiceNumber) AS InvoiceCount, SUM(InvoiceTotal) AS InvoiceSum
+FROM Invoices JOIN Vendors
+ON Invoices.VendorID = Vendors.VendorID
+GROUP BY VendorName
+ORDER BY COUNT(InvoiceNumber) DESC;
+
+--4.
+SELECT VendorName, COUNT(AccountNo) AS 'Total # of Accounts'
+FROM Vendors JOIN InvoiceLineItems
+ON Vendors.DefaultAccountNo = InvoiceLineItems.AccountNo
+GROUP BY VendorName;
+
+--5.
+SELECT VendorID, InvoiceDate, InvoiceTotal,
+	SUM(InvoiceTotal) OVER(PARTITION BY VendorID) AS VendorTotal,
+	COUNT(InvoiceNumber) OVER(PARTITION BY VendorID) AS VendorCount,
+	AVG(InvoiceTotal)OVER(PARTITION BY VendorID) AS VendorAvg
+FROM  Invoices;
+
+--6.
+SELECT InvoiceNumber, InvoiceTotal, PaymentTotal
+FROM Invoices
+WHERE PaymentTotal >(SELECT AVG(PaymentTotal) FROM Invoices
+	WHERE PaymentDate IS NOT NULL);
+
